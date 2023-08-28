99f7498410ac56fe205d968b9a8a9b0a4693b059
Rebecca Young <moonbladez@hotmail.com>
2023-08-28 21:40:04 +0200

    added graves

--- src/main.ts
+++ src/main.ts
@@ -62,6 +62,32 @@ floor.rotation.x = -Math.PI * 0.5;
 floor.position.y = 0;
 scene.add(floor);
 
+// Graves
+const graves = new THREE.Group();
+const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2);
+const graveMaterial = new THREE.MeshStandardMaterial({ color: "#b2b6b1" });
+const createGrave = (x: number, z: number): THREE.Mesh => {
+  const grave = new THREE.Mesh(graveGeometry, graveMaterial);
+
+  grave.position.set(x, 0.3, z);
+
+  grave.rotation.z = (Math.random() - 0.5) * 0.4;
+  grave.rotation.y = (Math.random() - 0.5) * 0.4;
+
+  return grave;
+};
+
+for (let i = 0; i < 25; i++) {
+  const angle = Math.random() * Math.PI * 2;
+  const radius = 3 + Math.random() * 6;
+  const x = Math.cos(angle) * radius;
+  const z = Math.sin(angle) * radius;
+
+  const grave = createGrave(x, z);
+  graves.add(grave);
+}
+scene.add(graves);
+
 const ambientLight = new THREE.AmbientLight("#ffffff", 0.5);
 scene.add(ambientLight);
 
